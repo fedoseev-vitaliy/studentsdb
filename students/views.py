54ff2766f767dcb5cd61866ae936c8f6703bfad9
fedoseev-vitaliy <fedoseevvt@gmail.com>
2015-10-08 17:41:52 +0300

    added views | dynamic urls in students_list.html | add template dir in settings

--- students/views.py
+++ students/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Students Views
+
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+
+def students_add(request):
+    return HttpResponse('<h1>Student add form</h1>')
+
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Student edit %s</h1>' % sid)
+
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Student delete %s</h1>' % sid)
+
+
+# Group Views
+
+
+def groups_list(request):
+    return HttpResponse('<h1>Group Listing</h1>')
+
+
+def groups_add(request):
+    return HttpResponse('<h1>Group add form</h1>')
+
+
+def groups_edit(request, sid):
+    return HttpResponse('<h1>Group edit %s</h1>' % sid)
+
+
+def groups_delete(request, sid):
+    return HttpResponse('<h1>Group delete %s</h1>' % sid)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,21 +1,25 @@
-"""studentsdb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
+from students import views 
+
 
 urlpatterns = [
+               
+               
+    #Students url
+    url(r'^$', views.students_list, name='home'),
+    url(r'^students/add/$', views.students_add, name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', views.students_edit, name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', views.students_delete, name='students_delete'),
+    
+    
+    #Group url    
+    url(r'^groups/$', views.groups_list, name='groups'),  
+    url(r'^groups/add/$', views.groups_add, name='groups_add'),
+    url(r'^groups/(?P<sid>\d+)/edit/$', views.students_edit, name='groups_edit'),
+    url(r'^groups/(?P<sid>\d+)/delete/$', views.students_delete, name='groups_delete'),
+    
+    
+    #Admin urls             
     url(r'^admin/', include(admin.site.urls)),
 ]
