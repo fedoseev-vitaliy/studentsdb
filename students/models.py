17d2417173427747be9aec5fd6daff2aa8ce3b88
fedoseev-vitaliy <fedoseevvt@gmail.com>
2015-10-13 17:18:08 +0300

    students mode created + add media root in settings

--- students/models.py
+++ students/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.template.defaultfilters import default
+
+class Student(models.Model):
+    """ Student model """
+    
+    first_name = models.CharField(max_lenght=256, blank=False, verbose_name=u'Имя')
+    last_name = models.CharField(max_lenght=256, blank=False, verbose_name=u'Призвище')
+    middle_name = models.CharField(max_lenght=256, blank=True, verbose_name=u'По-батькові', default='')
+    birth_day = models.DateField(blank=False, verbose_name=u'Дата народження', null=True)
+    photo = models.ImageField(blank=True, verbose_name=u'Фото', null=True)
+    ticket = models.CharField(max_lenght=256, blank=False, verbose_name=u'Білет')
+    notes = models.TextField(blank=True, verbose_name=u'Нотатки')
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -97,9 +97,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
